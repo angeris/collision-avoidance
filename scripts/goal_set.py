41fa0a3db0f64bebc957f4f734f1ee022e1dd893
Kunal Shah <kshah.kunal@gmail.com>
2021-04-19 12:11:06 -0700

    group goal set

--- scripts/goal_set.py
+++ scripts/goal_set.py
@@ -15,25 +15,42 @@ class Captian:
         rospy.init_node('poseCaptain', anonymous=True)
 
         # goal topic
-        self.goalPub = rospy.Publisher('command/goal', Pose, queue_size=10)
+        self.manifest = rospy.get_param("~manifest").split(",")
+        self.goalPubs = [rospy.Publisher(drone+'/command/goal',
+                         Pose, queue_size=10)
+                         for drone in self.manifest]
+        self.goals = {drone: Pose() for drone in self.manifest}
         # wait for connect
         rospy.sleep(2)
 
     def run(self):
 
         while not rospy.is_shutdown():
-
-            x, y, z = [float(val) for val in raw_input(
-                "Enter goal position: ").split()]
-
-            goalMsg = Pose()
-            # lazy unpack
-            goalMsg.position.x = x
-            goalMsg.position.y = y
-            goalMsg.position.z = z
+            print("enter cmd")
+            cmd = raw_input("u for update, g for go")
+            if cmd == "u":
+                self.setGoals()
+            elif cmd == "g":
+                self.sendGoals()
+            else:
+                ROS_WARN("invalid cmd")
             # publish
             self.goalPub.publish(goalMsg)
             rospy.sleep(.5)
+
+    def setGoals(self):
+        for drone in self.manifest:
+            print("enter goal for " + drone)
+            x, y, z = [float(val) for val in raw_input(
+                "Enter goal position: ").split()]
+            # save goal cmd
+            self.goals[drone].position.x = x
+            self.goals[drone].position.y = y
+            self.goals[drone].position.z = z
+
+    def sendGoals(self):
+        for drone, pub in zip(self.manifest, self.goalPub):
+            pub.publish(self.goals[drone])
 
 
 if __name__ == '__main__':
